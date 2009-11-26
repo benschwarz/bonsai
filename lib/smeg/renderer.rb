754b6d9cda7231f2c7b20b5f347007cac28308ba
Ben Schwarz <ben.schwarz@gmail.com>
2009-11-26 11:04:29 +1100

    Planning out the renderer

--- lib/smeg/renderer.rb
+++ lib/smeg/renderer.rb
@@ -1,5 +1,8 @@
 module Smeg
   module Renderer
-    
+    # Create destination output directory
+    # Get all the pages, render them and deposit under the correct directories
+    # Copy over anything from templates
+    # 
   end
 end
