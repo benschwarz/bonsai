a2ac1d3d4ac0425f8fbb8804b48ff429bbf183e6
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-01 09:19:35 +1100

    Removed for cleanup

--- lib/smeg/renderer.rb
+++
@@ -1,8 +0,0 @@
-module Smeg
-  module Renderer
-    # Create destination output directory
-    # Get all the pages, render them and deposit under the correct directories
-    # Copy over anything from templates
-    # 
-  end
-end
