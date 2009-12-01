500e47c00826d0603e87bf35404bb10cfc35af2a
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-01 16:07:14 +1100

    Implemented the exporter

--- lib/smeg/exporter.rb
+++ lib/smeg/exporter.rb
@@ -1,12 +1,59 @@
+require 'fileutils'
+
 module Smeg
   module Exporter
+    @@path = "output"
+    
     class << self
-      def path; @@path || "content"; end
+      def path; @@path; end
       def path=(path); @@path = path; end
+      
+      def publish!
+        teardown
+        setup
+        write_pages
+        write_index
+        copy_images
+        copy_public
+      end
+      
+      private 
+      def teardown
+        Smeg.log.info "Removing directory: #{path}"
+        FileUtils.rm_rf path
+      end
+
+      def setup
+        Smeg.log.info "Creating directory"
+        FileUtils.mkdir_p path
+      end
+      
+      def write_index
+        File.open("#{path}/index.html", "w") {|file| file.write(Page.find("index").render)}
+      end
+      
+      def write_pages
+        Page.all.each do |page|
+          FileUtils.mkdir_p("#{path}#{page.permalink}")
+          File.open("#{path}#{page.write_path}", "w"){|file| file.write(page.render) }
+        end
+      end
+      
+      def copy_images
+        Smeg.log.info "Copying images"
+        Page.all.each do |page|
+          Smeg.log.info "Coping images for #{page.slug}"
+          page.images.each do |image|
+            Smeg.log.info "Copying #{image[:name]} to #{path}#{page.permalink}"
+            FileUtils.cp "#{page.directory}/#{image[:name]}", "#{path}#{page.permalink}"
+          end
+        end
+      end
+      
+      def copy_public
+        Smeg.log.info "Copying public files"
+        Dir["#{Smeg.root_dir}/public/*"].each {|file| FileUtils.cp_r file, path }
+      end
     end
-    # Create destination output directory
-    # Get all the pages, render them and deposit under the correct directories
-    # Copy over anything from templates
-    # 
   end
 end
--- spec/smeg/exporter_spec.rb
+++ spec/smeg/exporter_spec.rb
@@ -1,6 +1,14 @@
 require "#{File.dirname(__FILE__)}/../spec_helper"
 
 describe Smeg::Exporter do
+  before :all do
+#    FakeFS.activate!
+  end
+  
+  after :all do
+#    FakeFS.deactivate!
+  end
+  
   it "should have a path" do
     Smeg::Exporter.path.should_not be_nil
   end
@@ -12,8 +20,51 @@ describe Smeg::Exporter do
   end
   
   describe "render" do
-    it "should create the output directory"
-    it "should get all pages"
-    it "should render all pages to the output directory"
+    before do
+      Smeg::Exporter.send(:teardown)
+    end
+    
+    it "should create the output directory" do
+      Smeg::Exporter.publish!
+      File.exists?(Smeg::Exporter.path).should be_true
+    end
+    
+    it "should remove the output directory before re-creating it" do
+      testfile = "#{Smeg::Exporter.path}/test"
+      
+      Smeg::Exporter.publish!
+      FileUtils.touch(testfile)
+      File.exists?(testfile).should be_true
+      Smeg::Exporter.publish!
+      File.exists?(testfile).should be_false
+    end
+        
+    it "should render all pages to the output directory" do
+      Smeg::Page.all{|page| File.exists?("#{Smeg::Exporter.path}#{page.write_path}").should be_false }
+      Smeg::Exporter.publish!
+      Smeg::Page.all{|page| File.exists?("#{Smeg::Exporter.path}#{page.write_path}").should be_true }
+    end
+    
+    it "should copy the images of each page to its directory" do  
+      Smeg.root_dir = "spec/support"
+      
+      File.exists?("#{Smeg::Exporter.path}/about-us/history/image001.jpg").should be_false
+      Smeg::Exporter.publish!
+      File.exists?("#{Smeg::Exporter.path}/about-us/history/image001.jpg").should be_true
+    end
+    
+    it "should copy the contents of the public directory to the root export path" do
+      Smeg.root_dir = "spec/support"
+      
+      File.exists?("#{Smeg::Exporter.path}/htaccess").should be_false
+      Smeg::Exporter.publish!
+      File.exists?("#{Smeg::Exporter.path}/htaccess").should be_true
+    end
+    
+    it "should write the index file to output/index.html" do
+      File.exists?("#{Smeg::Exporter.path}/index.html").should be_false
+      Smeg::Exporter.publish!
+      File.exists?("#{Smeg::Exporter.path}/index.html").should be_true
+    end
   end
 end
