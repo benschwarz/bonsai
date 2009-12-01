33ffe62a5186eea0a19fe8dcc9076700f69ffb0e
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-02 09:26:21 +1100

    Added "notes" for webservery stuff

--- lib/smeg/webserver.rb
+++ lib/smeg/webserver.rb
@@ -1,3 +1,8 @@
+# require 'rack/bug'
+# require 'rack/bug/panels/mustache_panel'
+# use Rack::Bug::MustachePanel
+# use Rack::Bug
+
 module Smeg
   class DevelopmentServer < Sinatra::Base
     set :public, Smeg.root_dir + "/output"
