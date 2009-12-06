7384f00dda4b858ecd2b3cc1594d3f8515cd7a5b
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-07 08:09:41 +1100

    Added some simple benchmarks to give an idea of render speeds

---
+++ benchmark/associations.rb
@@ -0,0 +1,45 @@
+require 'lib/smeg'
+require 'benchmark'
+
+Smeg.root_dir = File.dirname(__FILE__) + "/../spec/support"
+Smeg.configure {|config| config[:enable_logging] = false }
+
+page = Smeg::Page.find("about-us/history")
+
+Benchmark.bm do |b|
+  b.report "all" do
+    1000.times do
+      Smeg::Page.all
+    end
+  end
+  
+  b.report "find" do
+    10_000.times do
+      Smeg::Page.find("about-us/history")
+    end
+  end
+  
+  b.report "parent" do
+    10_000.times do
+      page.parent
+    end
+  end
+  
+  b.report "children" do
+    10_000.times do
+      page.children
+    end
+  end
+  
+  b.report "siblings" do
+    10_000.times do
+      page.siblings
+    end
+  end
+  
+  b.report "rendering" do
+    10_000.times do
+      page.render
+    end
+  end
+end
