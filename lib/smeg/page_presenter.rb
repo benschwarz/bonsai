9023c68fd7625c4fa96eb97e3bef81b4af6061de
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-07 21:14:18 +1100

    PagePresenter finally method_missing calls its proxied page object

--- lib/smeg/page_presenter.rb
+++ lib/smeg/page_presenter.rb
@@ -1,8 +1,11 @@
 module Smeg
   class PagePresenter < Mustache
+    attr_reader :page
+    
     def initialize(page)
       @page = page
-      self.template = @page.template.read
+      self.class.path = page.template.class.path + "/partials"
+      self.template = page.template.read
     end
         
     def navigation
@@ -10,30 +13,29 @@ module Smeg
     end
     
     def children
-      @page.children.map{|p| p.to_hash }
+      page.children.map{|p| p.to_hash }
     end
     
     def siblings
-      @page.siblings.map{|p| p.to_shallow_hash }
+      page.siblings.map{|p| p.to_shallow_hash }
     end
     
     def parent
-      @page.parent.nil? ? nil : @page.parent.to_shallow_hash
-    end
-
-    # Catch lookups to the template for variables
-    def [](message)
-      Smeg.log.debug("catching #{message} sent to []")
-      @page.send(message)
+      page.parent.nil? ? nil : @page.parent.to_shallow_hash
     end
 
     # Catch anything that wasn't picked up by local methods
     def method_missing(message, *args, &block)
-      Smeg.log.debug("catching #{message} sent to method_missing")
-      
-      return @page.send(message) 
-
-      #map_to_disk(message) || super
+      return page.send(message) if page.respond_to? message
+      map_to_disk(message)
+    end
+    
+    # Hack to make the PagePresenter class believe that it really
+    # can respond to the method regardless of weather it really can.
+    # method_missing picks up the peices and will return a nil result
+    # if nothing can be found.
+    def respond_to?(message)
+      true
     end
     
     private
