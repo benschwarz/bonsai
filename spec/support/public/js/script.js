9b6a0faa6940a5362ac495f286e00f17a35953fe
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-18 23:55:53 +1100

    A script to be used for compression specs

---
+++ spec/support/public/js/script.js
@@ -0,0 +1,19 @@
+// Dom ready
+$(function () {
+	// Search field
+	$('.default-value').each(function() {
+	    var default_value = this.value;
+	    $(this).focus(function() {
+	        if(this.value == default_value) {
+	            this.value = '';
+	        }
+	    });
+	    $(this).blur(function() {
+	        if(this.value == '') {
+	            this.value = default_value;
+	        }
+	    });
+	});
+	
+	$(".details dd:empty").hide().prev("dt").hide();
+});
