c74f8d4f4032caa2ec4b1844571fbe847a3d1509
Ben Schwarz <ben.schwarz@gmail.com>
2009-12-27 14:24:08 +1100

    Copying templates cleanly and creating directories that aren't introduced by templates

--- lib/bonsai/generate.rb
+++ lib/bonsai/generate.rb
@@ -4,24 +4,22 @@ module Bonsai
       @path = path
     
       Bonsai.log "Planting your bonsai '#{path}'"
-      create_directories
       copy_templates
+      create_directories
     end
   
-    private
-    def templates
-      File.expand_path("#{File.dirname(__FILE__)}/templates")
-    end
-  
+    private  
     def create_directories
-      %w(content content/index public/docs/css public/docs/js templates/partials).each do |dir|
+      # Create directories not created by the templates
+      %w(public/docs/js templates/partials).each do |dir|
         Bonsai.log "\tcreate\t#{dir}"
         FileUtils.mkdir_p(File.join(@path, dir))
       end
     end
   
     def copy_templates
-      FileUtils.cp_r("#{templates}", File.join(@path))
+      # Using system call because fileutils is inadequate
+      system("cp -fR '#{File.expand_path("#{File.dirname(__FILE__)}/templates")}' '#{@path}'")
     end
   end
 end
