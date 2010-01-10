f7471411d5715ecabd91448a02c439b091fdcb4a
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-10 12:07:20 +1100

    Added a simple site to be generated with --plant that features use of partials, navigation, children and ancestors. Specs to validate.

--- spec/bonsai/generate_spec.rb
+++ spec/bonsai/generate_spec.rb
@@ -37,12 +37,12 @@ describe Bonsai::Generate do
       File.exists?("#{@path}/site.yml").should be_true
     end
     
-    it "should generate 6 files" do
-      Dir.glob("#{@path}/**/*", File::FNM_DOTMATCH).select{|f| File.file?(f) }.size.should == 6
+    it "should generate 21 files" do
+      Dir.glob("#{@path}/**/*", File::FNM_DOTMATCH).select{|f| File.file?(f) }.size.should == 21
     end
     
-    it "should generate 7 directories" do
-      Dir.glob("#{@path}/**/*").select{|f| File.directory?(f) }.size.should == 7
+    it "should generate 16 directories" do
+      Dir.glob("#{@path}/**/*").select{|f| File.directory?(f) }.size.should == 16
     end
   end
 end
