fba170747f73343e6b0c74eed0e18a0fb3e2f155
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-16 18:27:53 +1100

    Tiny up

--- lib/bonsai/page.rb
+++ lib/bonsai/page.rb
@@ -150,8 +150,7 @@ module Bonsai
     
     private
     def formatted_content
-      formatted_content = content
-      
+      formatted_content = content      
       formatted_content.each do |k,v|
         if v.is_a?(String) and v =~ /\n/
           formatted_content[k] = to_markdown(v)
