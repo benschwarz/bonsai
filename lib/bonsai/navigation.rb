d7309582b190a4c73bcebccf67ada7a53ecef705
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-21 20:39:48 +1100

    An attempt to satisfy specs on non mac environments

--- lib/bonsai/navigation.rb
+++ lib/bonsai/navigation.rb
@@ -1,7 +1,7 @@
 module Bonsai
   class Navigation
     def self.tree
-      Page.all(Page.path, "*").select{|p| !p.floating? }
+      Page.all(Page.path, "*").select{|p| !p.floating? }.sort_by{|p| p.disk_path }
     end
   end
 end
