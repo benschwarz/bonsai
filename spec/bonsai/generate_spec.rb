f7a7ae62d113695f978a6fc3f8403c3978ce99bb
Ben Schwarz <ben.schwarz@gmail.com>
2010-12-28 21:43:49 +1100

    File count was incorrect. Hard to track this one down…

--- spec/bonsai/generate_spec.rb
+++ spec/bonsai/generate_spec.rb
@@ -37,12 +37,12 @@ describe Bonsai::Generate do
       File.exists?("#{@path}/site.yml").should be_true
     end
     
-    it "should generate 24 files" do
-      Dir.glob("#{@path}/**/*", File::FNM_DOTMATCH).select{|f| File.file?(f) }.size.should == 24
+    it "should generate 22 files" do
+      Dir.glob("#{@path}/**/*", File::FNM_DOTMATCH).select{|f| File.file?(f) }.size.should == 22
     end
     
-    it "should generate 16 directories" do
-      Dir.glob("#{@path}/**/*").select{|f| File.directory?(f) }.size.should == 16
+    it "should generate 14 directories" do
+      Dir.glob("#{@path}/**/*").select{|f| File.directory?(f) }.size.should == 14
     end
   end
 end
